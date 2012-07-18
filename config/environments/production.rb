3f2365daf92ec52913037780d07a321b398a419a
Ben McCanny <ben.mccanny@gmail.com>
2012-07-17 21:14:18 -0400

    Add production hostname to mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,6 @@ Announcements::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  config.action_mailer.default_url_options = { :host => "digest.skule.ca" }
 end
